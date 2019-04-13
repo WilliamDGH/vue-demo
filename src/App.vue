bc6691ddc5c54910b20cf39722235a179f2fe0f1
Guanhua <william.ting.1116@gmail.com>
2019-04-13 23:19:24 +1000

    first change

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js Demo App"/>
+    <HelloWorld msg="Welcome to Your Vue.js First Demo App"/>
   </div>
 </template>
 
